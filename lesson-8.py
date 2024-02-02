80ce244c5e4d626da5cacddfc1de8f85a7d06c15
intiqamquluzada <quluzadintiqam@gmail.com>
2024-02-02 10:58:45 +0400

    lesson-8

---
+++ lesson-8.py
@@ -0,0 +1,122 @@
+"""
+WHILE EXERCISES
+"""
+
+"""
+Daxil edilen cumlede yalniz sait herfleri cap eden python kodu yazin.
+"""
+# Input : Salam necesiz
+# Output: a a e e i
+
+# saitler = ['a', 'i', 'ı', 'e', 'o', 'u', 'ö', 'ü', 'ə']
+# sentence = input("Enter the sentence: ")
+# i = 0
+# while i < len(sentence):  # i = 0,1,2,3,4,5,6,7,8
+#     if sentence[i] in saitler:
+#         print(sentence[i])
+#     i += 1
+
+"""
+Daxil edilen 2 eded arasindaki cut ededleri cap edin.
+"""
+
+# num_1 = int(input("Enter first number: "))
+# num_2 = int(input("Enter second number: "))
+#
+# i = num_1
+# while i < num_2:
+#     if i % 2 == 0:
+#         print(i)
+#     i += 1
+
+
+"""
+Daxil edilen ededin reqemlerini cap edin.
+"""
+# number = input('Enter the number: ') # 3564
+#
+# i = 0
+#
+# while i < len(number):
+#     print(number[i])
+#
+#     i += 1
+
+
+"""
+Daxil edilen ededin reqemlerinin cemini tapin.
+"""
+
+
+# number = input("Enter the number: ")# 222
+#
+# sum_of_digits = 0
+#
+# i = 0
+# while i < len(number): # i < 3
+#     sum_of_digits += int(number[i]) # 2, 4, 6
+#     i += 1
+# print(sum_of_digits)
+
+
+"""
+Istifadeciden parol isteyin, parol dogru olana qeder proqram dayanmasin.
+"""
+
+# while True:
+#     password = input('Enter the password: ')
+#     if password == 'edustar2024':
+#         print('Siz daxil oldunuz')
+#         break
+
+# i = 0
+# while i < 10:
+#     print('Hello World')
+#     i += 1
+
+
+"""
+Istifadeciden alinan ededin bolenlerini ekrana cap edin.
+"""
+
+# a = int(input('Enter the number: '))
+#
+# i = 1
+#
+# while i <= a:
+#     if a % i == 0:
+#         print(i)
+#     i += 1
+
+
+"""
+Daxil edilen herf boyukdurse kicilden, kicikdirse boyuden python kodu yazin.
+"""
+
+# alpha = input('Enter the alpha: ')
+#
+# if alpha == alpha.upper():
+#     print(alpha.lower())
+# else:
+#     print(alpha.upper())
+
+
+"""
+Daxil edilen cumlede eded yaxud reqem varsa cap eden python kodu yazin.
+"""
+
+# Input: Salam5 necesiz 2
+# Output: 5 2
+
+
+"""
+ORD , CHR
+"""
+# my_list = []
+# i = 65
+# while i <= 90:
+#     my_list.append(chr(i))
+#
+#     i += 1
+# print(my_list)
+# print(ord('A'))
